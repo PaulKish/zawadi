29813eacd6476d19a67aa4ffe1828101d724fcdd
Paul Ngumii <paulngumii@gmail.com>
2016-10-11 21:33:07 +0300

    Css changes

--- views/layout/backend.php
+++ views/layout/backend.php
@@ -17,13 +17,22 @@
 </head>
 <body>
   	<div class="container about">
+  		<div class="row">
+			<div class="four columns">
+		  		<a href="www.zawadiafrica.org">
+		  			<img class="u-max-full-width" src="public/img/zawadi_logo.png">
+		  		</a>
+		  	</div>
+		</div>
+		<hr>
+
   		<?php foreach(Flight::flash()->all() as $flash):?>
   			<div class="row">
 		    	<span class="<?= $flash['type']; ?>"><?= $flash['message'] ?></span>
 		    </div>
 		<?php endforeach; ?>
 		<?php Flight::flash()->clear(); ?>
-		
+
 		<?= $body_content; ?>
   	</div>
 </body>
