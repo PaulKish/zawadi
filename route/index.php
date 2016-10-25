c534753e136726fa3735683e4fa45e253a99c937
Paul Ngumii <paulngumii@gmail.com>
2016-10-25 13:37:24 +0300

    Tabs v spaces

--- route/index.php
+++ route/index.php
@@ -3,41 +3,41 @@
 // landing page route
 Flight::route('/', function(){
 	// render partial
-    Flight::render('frontend/index',[],'body_content');
+	Flight::render('frontend/index',[],'body_content');
 
-    // render to layout
-    Flight::render('layout/frontend');
+	// render to layout
+	Flight::render('layout/frontend');
 });
 
 // donate
 Flight::route('POST /index', function(){
-    $request = Flight::request();
-    
-    $data['donation_type'] = $request->data->donation_type;
-    $data['amount'] = $request->data->amount;
-    $data['payment_installment'] = $request->data->payment_installment;
-    $data['payment_method'] = $request->data->payment_method;
-    $data['name'] = $request->data->name;
-    $data['physical_address'] = $request->data->physical_address;
-    $data['email'] = $request->data->email;
-    $data['tel_cell'] = $request->data->tel_cell;
-    $data['anonymous'] = $request->data->anonymous;
+	$request = Flight::request();
+	
+	$data['donation_type'] = $request->data->donation_type;
+	$data['amount'] = $request->data->amount;
+	$data['payment_installment'] = $request->data->payment_installment;
+	$data['payment_method'] = $request->data->payment_method;
+	$data['name'] = $request->data->name;
+	$data['physical_address'] = $request->data->physical_address;
+	$data['email'] = $request->data->email;
+	$data['tel_cell'] = $request->data->tel_cell;
+	$data['anonymous'] = $request->data->anonymous;
 
-  	$fpdo = Flight::fpdo();
-    
-  	/*
-  	$check_exist = $fpdo->from('donors')->where('email',$data['email'])->fetch();
-  	if($check_exist){
-  		Flight::flash()->error('Donor email exists');
-  		Flight::redirect('/');
-  	}*/
+	$fpdo = Flight::fpdo();
+	
+	/*
+	$check_exist = $fpdo->from('donors')->where('email',$data['email'])->fetch();
+	if($check_exist){
+		Flight::flash()->error('Donor email exists');
+		Flight::redirect('/');
+	}*/
 
-  	$insert = $fpdo->insertInto('donors')->values($data)->execute();
-  	if($insert){
-  		Flight::flash()->success('Donation pledge has been added. Thank you for your support');
-  	}else{
-  		Flight::flash()->error('Please try again');
-  	}
+	$insert = $fpdo->insertInto('donors')->values($data)->execute();
+	if($insert){
+		Flight::flash()->success('Donation pledge has been added. Thank you for your support');
+	}else{
+		Flight::flash()->error('Please try again');
+	}
 
-  	Flight::redirect('/');        
+	Flight::redirect('/');        
 });
